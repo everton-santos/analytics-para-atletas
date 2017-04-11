eccf0d12632abd4dd99802ff1b8f438c50ab243b
everton-santos <everton26se@gmail.com>
2017-04-11 01:52:21 -0300

    adicionado comentarios

--- src/app/servicos/atleta.service.ts
+++ src/app/servicos/atleta.service.ts
@@ -15,23 +15,37 @@ export class AtletaService {
 
   }
 
+  /**
+   * Retorna observable de atletas de um esporte especifico
+   * 
+   * @param esporte 
+   */
   public getAtletas(esporte: string): Observable<Array<Atleta>> {
 
+    //cria o observable a ser retornado como resultado
     let result = new Observable<Array<Atleta>>(observer => {
 
+      //cria lista a ser populada com o retorno do http
       let lista = new Array<Atleta>();
 
+      //faz requisição http da url do servidor retornando 10 resultados
       this.http.get(this.urlServico + '?results=10')
+        //trata o resultado como um observable utilizando o subscribe
         .subscribe(response => {
+          //passa o resultado para objeto utilizando o "json()" 
+          //faz um foreach na propriedade results
           response.json().results.forEach(element => {
+            //adiciona cada elemento na lista 
+            //transformando em um objeto da classe Atleta
             lista.push({
-              nome: `${element.name.first} ${element.name.last}`,
-              sexo: element.gender ==  'male' ? 'masculino' : 'feminino',
-              dataNascimento: new Date(element.dob),
-              esporte: esporte
+              nome: `${element.name.first} ${element.name.last}`, //utilizando javascript template string para formatar o nome do atleta
+              sexo: element.gender == 'male' ? 'masculino' : 'feminino', //utilizando if ternario para tratar o genero do atleta deixando-o em portugues
+              dataNascimento: new Date(element.dob), //transforma a data de string para objeto de Date
+              esporte: esporte //atribui o esporte do atleta de acordo com o esporte passado como parametro 
             })
           });
 
+          //conclui tarefa chamando o next 
           observer.next(lista)
 
         })
